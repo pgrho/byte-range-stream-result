6fdeb9321ef046027cdb59e6bc9bf78a6185680d
pgrho <shipwreck@live.jp>
2017-08-02 17:46:51 +0900

    Initial commit

---
+++ src/Shipwreck.AspNet.Mvc.ByteRangeStreamResult/ByteRange.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+
+#if ASPNET_CORE
+namespace Shipwreck.AspNetCore
+#else
+namespace Shipwreck.AspNet.Mvc
+#endif
+{
+    /// <summary>
+    /// Represents a byte range of HTTP Range header.
+    /// </summary>
+    public struct ByteRange : IEquatable<ByteRange>
+    {
+        #region Fields
+
+        private readonly int _FirstIndex;
+
+        private readonly int _LastIndexOrSuffixLength;
+
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// 開始バイトと終了バイトの<c>0</c>から始まるインデックスを指定して<see cref="ByteRange"/>構造体の新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="firstIndex">開始バイトの<c>0</c>から始まるインデックス。</param>
+        /// <param name="lastIndexOrNegative">終了バイトの<c>0</c>から始まるインデックス。終了バイトを指定しない場合は負の値。</param>
+        public ByteRange(int firstIndex, int lastIndexOrNegative)
+        {
+            if (firstIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(firstIndex));
+            }
+
+            _FirstIndex = firstIndex;
+
+            if (lastIndexOrNegative < 0)
+            {
+                _LastIndexOrSuffixLength = -1;
+            }
+            else
+            {
+                if (lastIndexOrNegative < firstIndex)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(lastIndexOrNegative));
+                }
+                else
+                {
+                    _LastIndexOrSuffixLength = lastIndexOrNegative;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 送信する範囲のファイル末尾からのバイト数を指定して<see cref="ByteRange"/>構造体の新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="suffixLength">送信するファイル末尾からのバイト数。</param>
+        public ByteRange(int suffixLength)
+        {
+            if (suffixLength <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(suffixLength));
+            }
+
+            _FirstIndex = -1;
+            _LastIndexOrSuffixLength = suffixLength;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// 現在の範囲が末尾からのバイト数指定であるかどうかを示す値を取得します。
+        /// </summary>
+        public bool IsSuffixLength => _FirstIndex < 0;
+
+        /// <summary>
+        /// 現在の範囲の<c>0</c>から始まる開始バイトを取得します。
+        /// </summary>
+        /// <exception cref="InvalidOperationException">現在の範囲が末尾からのバイト数指定である場合。</exception>
+        public int FirstIndex
+        {
+            get
+            {
+                if (IsSuffixLength)
+                {
+                    throw new InvalidOperationException();
+                }
+                return _FirstIndex;
+            }
+        }
+
+        /// <summary>
+        /// 現在の範囲の<c>0</c>から始まる終了バイトを取得します。
+        /// </summary>
+        /// <exception cref="InvalidOperationException">現在の範囲が末尾からのバイト数指定である場合。</exception>
+        public int? LastIndex
+        {
+            get
+            {
+                if (IsSuffixLength)
+                {
+                    throw new InvalidOperationException();
+                }
+                if (_LastIndexOrSuffixLength < 0)
+                {
+                    return null;
+                }
+                return _LastIndexOrSuffixLength;
+            }
+        }
+
+        /// <summary>
+        /// 現在の範囲のファイル末尾からのバイト数を取得します。
+        /// </summary>
+        /// <exception cref="InvalidOperationException">現在の範囲が末尾からのバイト数指定でない場合。</exception>
+        public int SuffixLength
+        {
+            get
+            {
+                if (!IsSuffixLength)
+                {
+                    throw new InvalidOperationException();
+                }
+                return _LastIndexOrSuffixLength;
+            }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        #region Operator Overloads
+
+        /// <summary>
+        /// 2つのバイトの範囲が等しいかどうかを示す値を返します。
+        /// </summary>
+        /// <param name="left">比較対象の最初のバイトの範囲。</param>
+        /// <param name="right">比較対象の2番目のバイトの範囲。</param>
+        /// <returns><paramref name="left" />と<paramref name="right" />が同じ値の場合は<c>true</c>。それ以外の場合は<c>false</c>。</returns>
+        public static bool operator ==(ByteRange left, ByteRange right)
+            => left._FirstIndex == right._FirstIndex && left._LastIndexOrSuffixLength == right._LastIndexOrSuffixLength;
+
+        /// <summary>
+        /// 2つのバイトの範囲が等しくないかどうかを示す値を返します。
+        /// </summary>
+        /// <param name="left">比較対象の最初のバイトの範囲。</param>
+        /// <param name="right">比較対象の2番目のバイトの範囲。</param>
+        /// <returns><paramref name="left" />と<paramref name="right" />が異なる値の場合は<c>true</c>。それ以外の場合は<c>false</c>。</returns>
+        public static bool operator !=(ByteRange left, ByteRange right)
+            => left._FirstIndex != right._FirstIndex || left._LastIndexOrSuffixLength != right._LastIndexOrSuffixLength;
+
+        #endregion Operator Overloads
+
+        #region Range Parsing Methods
+
+        /// <summary>
+        /// バイトの範囲の文字列形式をそれと等価な<see cref="ByteRange" />に変換します。
+        /// </summary>
+        /// <param name="s">変換するバイトの範囲を格納する文字列。</param>
+        /// <returns><paramref name="s"/>に格納されたバイトの範囲と等しい<see cref="ByteRange"/>。</returns>
+        public static ByteRange Parse(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+            ByteRange r;
+            if (!TryParse(s, out r))
+            {
+                throw new FormatException();
+            }
+            return r;
+        }
+
+        /// <summary>
+        /// バイトの範囲の文字列形式をそれと等価な<see cref="ByteRange" />に変換します。戻り値は変換が成功したかどうかを示します。
+        /// </summary>
+        /// <param name="s">変換するバイトの範囲を格納する文字列。</param>
+        /// <param name="result">
+        /// 変換が成功した場合、<paramref name="s"/>に格納されたバイトの範囲と等しい<see cref="ByteRange"/>を格納します。
+        /// 変換に失敗した場合は<see cref="ByteRange"/>の既定値を格納します。</param>
+        /// <returns><paramref name="s"/>が正常に変換された場合は<c>true</c>。それ以外の場合は<c>false</c>。</returns>
+        public static bool TryParse(string s, out ByteRange result)
+        {
+            if (s == null)
+            {
+                result = default(ByteRange);
+                return false;
+            }
+
+            return TryParse(s, 0, s.Length, out result);
+        }
+
+        /// <summary>
+        /// 文字列の一部分に格納されたバイトの範囲の文字列形式をそれと等価な<see cref="ByteRange" />に変換します。戻り値は変換が成功したかどうかを示します。
+        /// </summary>
+        /// <param name="s">変換するバイトの範囲を格納する文字列。</param>
+        /// <param name="startIndex">変換範囲の最初の文字の<c>0</c>から始まるインデックス。</param>
+        /// <param name="length">変換範囲の文字数。</param>
+        /// <param name="result">
+        /// 変換が成功した場合、<paramref name="s"/>の指定された範囲に格納されたバイトの範囲と等しい<see cref="ByteRange"/>を格納します。
+        /// 変換に失敗した場合は<see cref="ByteRange"/>の既定値を格納します。</param>
+        /// <returns><paramref name="s"/>の指定された範囲が正常に変換された場合は<c>true</c>。それ以外の場合は<c>false</c>。</returns>
+        public static bool TryParse(string s, int startIndex, int length, out ByteRange result)
+        {
+            if (s == null)
+            {
+                result = default(ByteRange);
+                return false;
+            }
+            var mi = s.IndexOf('-', startIndex);
+            var ci = startIndex + length;
+
+            if (startIndex < 0 || length <= 0 || startIndex + length - 1 >= s.Length || mi < -1 || mi > ci)
+            {
+                result = default(ByteRange);
+                return false;
+            }
+            else if (mi == startIndex)
+            {
+                int sl;
+                if (!TryParseInt32(s, startIndex + 1, ci - startIndex - 1, out sl) || sl <= 0)
+                {
+                    result = default(ByteRange);
+                    return false;
+                }
+                result = new ByteRange(sl);
+                return true;
+            }
+            else if (mi == ci - 1)
+            {
+                int sl;
+                if (!TryParseInt32(s, startIndex, ci - startIndex - 1, out sl))
+                {
+                    result = default(ByteRange);
+                    return false;
+                }
+                result = new ByteRange(sl, -1);
+                return true;
+            }
+            else
+            {
+                int f, l;
+                if (!TryParseInt32(s, startIndex, mi - startIndex, out f)
+                    || !TryParseInt32(s, mi + 1, ci - mi - 1, out l)
+                    || f > l)
+                {
+                    result = default(ByteRange);
+                    return false;
+                }
+                result = new ByteRange(f, l);
+                return true;
+            }
+        }
+
+        private static bool TryParseInt32(string s, int startIndex, int length, out int result)
+        {
+            if (startIndex < 0 || length <= 0 || startIndex + length - 1 >= s.Length)
+            {
+                result = 0;
+                return false;
+            }
+
+            var b = 0L;
+            for (var i = 0; i < length; i++)
+            {
+                var c = s[startIndex + i];
+                if ('0' <= c && c <= '9')
+                {
+                    b = b * 10 + c - '0';
+                    if (b > int.MaxValue)
+                    {
+                        result = 0;
+                        return false;
+                    }
+                }
+                else
+                {
+                    result = 0;
+                    return false;
+                }
+            }
+
+            result = (int)b;
+            return true;
+        }
+
+        #endregion Range Parsing Methods
+
+        #region Header Parsing Methods
+
+        /// <summary>
+        /// HTTP <c>Range</c>ヘッダーの値をそれと等価な<see cref="ByteRange" />のリストに変換します。
+        /// </summary>
+        /// <param name="headerValue">変換するHTTP <c>Range</c>ヘッダーを格納する文字列</param>
+        /// <returns><paramref name="headerValue"/>に格納されたバイトの範囲のリスト。</returns>
+        public static List<ByteRange> ParseHeader(string headerValue)
+        {
+            if (headerValue == null)
+            {
+                throw new ArgumentNullException(nameof(headerValue));
+            }
+            List<ByteRange> r;
+            if (!TryParseHeader(headerValue, out r))
+            {
+                throw new FormatException();
+            }
+            return r;
+        }
+
+        /// <summary>
+        /// HTTP <c>Range</c>ヘッダーの値をそれと等価な<see cref="ByteRange" />のリストに変換します。戻り値は変換が成功したかどうかを示します。
+        /// </summary>
+        /// <param name="headerValue">変換するHTTP <c>Range</c>ヘッダーを格納する文字列</param>
+        /// <param name="result">
+        /// 変換が成功した場合、<paramref name="headerValue"/>に格納されたバイトの範囲のリストを格納します。
+        /// 変換に失敗した場合は<c>null</c>を格納します。</param>
+        /// <returns><paramref name="headerValue"/>が正常に変換された場合は<c>true</c>。それ以外の場合は<c>false</c>。</returns>
+
+        public static bool TryParseHeader(string headerValue, out List<ByteRange> result)
+        {
+            var r = new List<ByteRange>(1);
+            if (TryParseHeader(headerValue, r))
+            {
+                result = r;
+                return true;
+            }
+            result = null;
+            return false;
+        }
+
+        private static bool TryParseHeader(string headerValue, ICollection<ByteRange> result)
+        {
+            if (headerValue?.StartsWith("bytes=") != true)
+            {
+                return false;
+            }
+
+            var si = 6;
+
+            while (si < headerValue.Length)
+            {
+                var ci = headerValue.IndexOf(',', si);
+                ci = ci < 0 ? headerValue.Length : ci;
+
+                ByteRange br;
+                if (!TryParse(headerValue, si, ci - si, out br))
+                {
+                    return false;
+                }
+
+                result.Add(br);
+
+                si = ci + 1;
+            }
+
+            return true;
+        }
+
+        #endregion Header Parsing Methods
+
+        #region Instance Methods
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+            => obj is ByteRange && this == (ByteRange)obj;
+
+        /// <summary>
+        /// 現在のインスタンスの値と指定したバイトの範囲の値が等しいかどうかを示す値を返します。
+        /// </summary>
+        /// <param name="other">比較対象のバイトの範囲。</param>
+        /// <returns>
+        /// このバイトの範囲の値と<paramref name="other" />の値が等しい場合はtrue。
+        /// それ以外の場合はfalse。
+        /// </returns>
+        public bool Equals(ByteRange other) => this == other;
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+            => unchecked((short)_FirstIndex ^ (((short)_LastIndexOrSuffixLength) << 16));
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            if (IsSuffixLength)
+            {
+                return $"-{_LastIndexOrSuffixLength}";
+            }
+            if (_LastIndexOrSuffixLength < 0)
+            {
+                return $"{_FirstIndex}-";
+            }
+            return $"{_FirstIndex}-{_LastIndexOrSuffixLength}";
+        }
+
+        #endregion Instance Methods
+
+        #endregion Methods
+    }
+
+
+}
---
+++ src/Shipwreck.AspNet.Mvc.ByteRangeStreamResult/ByteRangeStreamResult.cs
@@ -0,0 +1,211 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using Shipwreck.Web.Rfc;
+#if ASPNET_CORE
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Extensions;
+#else
+using System.Web;
+using System.Web.Mvc;
+#endif
+
+#if ASPNET_CORE
+namespace Shipwreck.AspNetCore
+#else
+namespace Shipwreck.AspNet.Mvc
+#endif
+{
+    /// <summary>
+    ///  <see cref="Stream"/>インスタンスを使用して、バイナリ コンテンツの指定されたバイトの範囲を応答に送信します。
+    /// </summary>
+    /// <seealso href="https://httpwg.org/specs/rfc7233.html">
+    /// RFC 7233 Hypertext Transfer Protocol (HTTP/1.1): Range Requests
+    /// </seealso>
+    public class ByteRangeStreamResult
+#if ASPNET_CORE
+        : IActionResult
+#else
+        : ActionResult
+#endif
+    {
+        /// <summary>
+        /// 応答に送信するストリームです。
+        /// </summary>
+        private readonly Stream _Stream;
+
+        /// <summary>
+        /// 応答に送信するファイルとコンテンツ タイプを指定して<see cref="ByteRangeStreamResult" />クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="fileName">応答に送信するファイルの名前。</param>
+        /// <param name="contentType">応答に使用するコンテンツ タイプ。</param>
+        public ByteRangeStreamResult(string fileName, string contentType)
+            : this(new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read), contentType)
+        {
+        }
+
+        /// <summary>
+        /// 応答に送信するストリームとコンテンツ タイプを指定して<see cref="ByteRangeStreamResult" />クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="stream">応答に送信するストリーム。</param>
+        /// <param name="contentType">応答に使用するコンテンツ タイプ。</param>
+        public ByteRangeStreamResult(Stream stream, string contentType)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+            if (!stream.CanRead || !stream.CanSeek)
+            {
+                throw new ArgumentException($"{nameof(stream)}で読み取りおよびシークがサポートされていません。");
+            }
+
+            _Stream = stream;
+            ContentType = contentType;
+        }
+
+        /// <summary>
+        /// 応答に使用するコンテンツ タイプを取得します。
+        /// </summary>
+        public string ContentType { get; }
+
+        /// <summary>
+        /// ファイル ダウンロード ダイアログ ボックスが、指定されたファイル名と共にブラウザーに表示されるように、Content-Disposition ヘッダーを取得または設定します。
+        /// </summary>
+        public string FileDownloadName { get; set; }
+
+        /// <summary>
+        /// バイトの範囲を送信するかどうかを示す値を取得または設定します。
+        /// </summary>
+        public bool AcceptRanges { get; set; } = true;
+
+        /// <summary>
+        /// クライアント側の切断を無視するかどうかを示す値を取得または設定します。
+        /// </summary>
+        public bool IgnoreDisconnection { get; set; }
+
+
+#if ASPNET_CORE
+        public async Task ExecuteResultAsync(ActionContext context)
+#else
+        /// <inheritdoc />
+        public override void ExecuteResult(ControllerContext context)
+#endif
+        {
+            using (_Stream)
+            {
+                var res = context.HttpContext.Response;
+
+                var rh = context.HttpContext.Request.Headers["Range"];
+
+                List<ByteRange> ranges;
+
+                var start = -1L;
+                var length = -1L;
+                if (AcceptRanges && ByteRange.TryParseHeader(rh, out ranges) && ranges.Count == 1)
+                {
+                    // Single byte range
+
+                    var r = ranges[0];
+
+                    if (r.IsSuffixLength)
+                    {
+                        length = r.SuffixLength;
+                        start = _Stream.Length - length;
+                    }
+                    else
+                    {
+                        start = r.FirstIndex;
+                        length = (r.LastIndex + 1 ?? _Stream.Length) - start;
+                    }
+
+                    if (0 > start || start >= _Stream.Length
+                        || length <= 0 || start + length > _Stream.Length)
+                    {
+                        start = -1;
+                    }
+                }
+
+                if (start >= 0)
+                {
+                    res.StatusCode = 206;
+#if !ASPNET_CORE
+                    res.StatusDescription = "Partial Content";
+#endif
+                    res.ContentType = ContentType ?? "application/octet-stream";
+                    res.Headers["Content-Length"] = length.ToString();
+                    res.Headers["Content-Range"] = $"bytes {start}-{start + length - 1}/{_Stream.Length}";
+                }
+                else
+                {
+                    // No Range Specified or Not Supported format
+                    res.StatusCode = 200;
+#if !ASPNET_CORE
+                    res.StatusDescription = "OK";
+#endif
+                    res.ContentType = ContentType ?? "application/octet-stream";
+                    res.Headers["Content-Length"] = _Stream.Length.ToString();
+                    if (FileDownloadName != null)
+                    {
+                        res.Headers["Content-Disposition"] = Rfc6266.GetContentDispositionHeader(FileDownloadName);
+                    }
+                    start = 0;
+                    length = _Stream.Length;
+                }
+                if (AcceptRanges)
+                {
+                    res.Headers["Accept-Ranges"] = "bytes";
+                }
+
+
+#if ASPNET_CORE
+                if (context.HttpContext.Request.Method == "HEAD")
+#else
+                  if (context.HttpContext.Request         .HttpMethod   == "HEAD")
+#endif
+                {
+                    return;
+                }
+                var buff = new byte[Math.Min(length, 4096)];
+
+                _Stream.Position = start;
+
+                try
+                {
+#if ASPNET_CORE
+                    await StreamCopyOperation.CopyToAsync(_Stream, context.HttpContext.Response.Body, length, context.HttpContext.RequestAborted).ConfigureAwait(false);
+
+#else
+                        while (res.IsClientConnected && length > 0)          
+                    {
+                        var l = _Stream.Read(buff, 0, (int)Math.Min(length, buff.Length));
+                        if (l <= 0)
+                        {
+                            break;
+                        }
+                        length -= l;
+                        res.OutputStream.Write(buff, 0, l);
+                        res.Flush();
+                    }   
+#endif
+                }
+#if ASPNET_CORE
+                catch
+                {
+#else
+                catch (HttpException ex)
+                {
+                    if (IgnoreDisconnection
+                        && ex.ErrorCode == unchecked((int)0x800703E3))
+                    {
+                        return;
+                    }
+#endif
+                    throw;
+                }
+            }
+        }
+    }
+}
